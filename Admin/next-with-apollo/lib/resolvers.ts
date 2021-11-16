714d7a005b015113049f433a0328f1f7132be41c
BasterCz <logimage110@seznam.cz>
2021-11-16 19:30:20 +0100

    MongoDB connect using Apollo/graph-ql NW

---
+++ Admin/next-with-apollo/lib/resolvers.ts
@@ -0,0 +1,49 @@
+import { QueryResolvers, MutationResolvers } from './type-defs.graphqls'
+import { ResolverContext } from './apollo'
+import { playlistAdd, playlistAddSong, playlistDelete, playlistGet, playlistGetOne, playlistRemoveSong, playlistSongs, playlistUpdate, songsAdd, songsDelete, songsGet, songsGetOne, songsUpdate, } from '../extensions/api/mongoapi'
+
+const Query: Required<QueryResolvers<ResolverContext>> = {
+  async songs(_parent, args, _context, _info) {
+    return await songsGet();
+  },
+  async playlists(_parent, args, _context, _info) {
+    return await playlistGet();
+  },
+  async song(_parent, args, _context, _info) {
+    return await songsGetOne(args._id);
+  },
+  async playlist(_parent, args, _context, _info) {
+    return await playlistGetOne(args._id);
+  },
+  async playlistSongs(_parent, args, _context, _info) {
+    return await playlistSongs(args._id);
+  },
+}
+
+const Mutation: Required<MutationResolvers<ResolverContext>> = {
+  async songsUpdate(_parent, args, _context, _info) {
+    return await songsUpdate(args._id, args.name, args.file_path, args.image_path, args.isPublic);
+  },
+  async playlistUpdate(_parent, args, _context, _info) {
+    return await playlistUpdate(args._id, args.name, args.description, args.songs, args.image_path, args.isPublic);
+  },
+  async songsAdd(_parent, args, _context, _info) {
+    return await songsAdd(args.name, args.file_path, args.image_path, args.isPublic);
+  },
+  async playlistAdd(_parent, args, _context, _info) {
+    return await playlistAdd(args.name, args.description, args.songs, args.image_path, args.isPublic);
+  },
+  async songsDelete(_parent, args, _context, _info) {
+    return await songsDelete(args._id)
+  },
+  async playlistDelete(_parent, args, _context, _info) {
+    return await playlistDelete(args._id);
+  },
+  async playlistAddSong(_parent, args, _context, _info) {
+    return await playlistAddSong(args._id, args.song);
+  },
+  async playlistRemoveSong(_parent, args, _context, _info) {
+    return await playlistRemoveSong(args._id, args.song);
+  },
+}
+export default { Query, Mutation }
--- Admin/next-with-apollo/next.config.js
+++ Admin/next-with-apollo/next.config.js
@@ -1,4 +1,37 @@
+const withNextCircularDeps = require('next-circular-dependency');
+const withCSS = require('@zeit/next-css');
+const optimizedImages = require('next-optimized-images');
+
+const circularDep = withNextCircularDeps(optimizedImages(withCSS({
+  exclude: /node_modules/,
+  failOnError: false,
+})))
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.graphql$/,
+      exclude: /node_modules/,
+      use: [options.defaultLoaders.babel, { loader: 'graphql-let/loader' }],
+    })
+
+    config.module.rules.push({
+      test: /\.graphqls$/,
+      exclude: /node_modules/,
+      use: ['graphql-let/schema/loader'],
+    })
+
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      type: 'json',
+      use: 'yaml-loader',
+    })
+
+    
+
+    return config
+  },
+  circularDep,
 };
